6c3f9ae5c8d18c94ffa5ec52e8943567903cba5b
amuza2 <mohamuza@gmail.com>
2025-08-31 17:45:34 +0100

    Initial

---
+++ ProductivityCake/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductivityCake.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ProductivityCake/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProductivityCake.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
